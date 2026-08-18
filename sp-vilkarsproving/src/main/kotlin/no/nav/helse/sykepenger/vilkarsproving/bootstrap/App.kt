8135929a47e49e3740310515bfcba0faedc4ac5e
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-08-18 14:28:53 +0200

    ✨ Koble på database + sql-migrering
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- sp-vilkarsproving/src/main/kotlin/no/nav/helse/sykepenger/vilkarsproving/bootstrap/App.kt
+++ sp-vilkarsproving/src/main/kotlin/no/nav/helse/sykepenger/vilkarsproving/bootstrap/App.kt
@@ -1,14 +1,44 @@
 package no.nav.helse.sykepenger.vilkarsproving.bootstrap
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.ApplicationStarted
 import no.nav.helse.rapids_rivers.RapidApplication
+import no.nav.helse.sykepenger.vilkarsproving.shared.logging.loggInfo
+import org.flywaydb.core.Flyway
 
 fun main() {
-    launchApplication(System.getenv())
+    val env = System.getenv()
+    val dataSource =
+        HikariDataSource(
+            HikariConfig().apply {
+                jdbcUrl = env.getValue("DATABASE_JDBC_URL")
+                maximumPoolSize = 10
+            },
+        )
+    launchApplication(System.getenv(), dataSource)
 }
 
-fun launchApplication(env: Map<String, String>) {
+fun launchApplication(
+    env: Map<String, String>,
+    dataSource: HikariDataSource,
+) {
     RapidApplication
-        .create(env)
+        .create(env, builder = {
+            withKtorModule {
+                monitor.subscribe(ApplicationStarted) {
+                    loggInfo("Migrerer database")
+                    Flyway
+                        .configure()
+                        .dataSource(dataSource)
+                        .cleanDisabled(true)
+                        .lockRetryCount(-1)
+                        .load()
+                        .migrate()
+                    loggInfo("Migrering ferdig!")
+                }
+            }
+        })
         .apply {
             VilkårsprøvingModule(this)
         }.start()
